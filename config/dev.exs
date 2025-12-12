0a630d578bee99a95fdb9380924bf01ceec4b109
lalabuy948 <Lalabuy9948@gmail.com>
2025-12-12 11:33:35 +0100

    0.1.1

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :polyx, Polyx.Repo,
-  database: Path.expand("../polyx_dev.db", __DIR__),
+  database: Path.expand("../polyx.db", __DIR__),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
